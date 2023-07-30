bf99087531f6270bfbb30e92cd2cd6f0a040f822
GorleSaratSai <140937178+GorleSaratSai@users.noreply.github.com>
2023-07-30 11:55:06 +0530

    Add files via upload

---
+++ Bridge interfaces/apb_interface.v
@@ -0,0 +1,29 @@
+module apb_interface(pwrite,penable,pselx,paddr,pwdata,pwriteout,penableout,pselxout,paddrout,pwdataout,prdata);
+
+input pwrite,penable;
+input [2:0] pselx;
+input [31:0] paddr;
+input [31:0] pwdata;
+
+output pwriteout;
+output penableout;
+output [2:0] pselxout;
+output [31:0] paddrout;
+output [31:0] pwdataout;
+output reg [31:0] prdata;
+
+assign pwriteout =pwrite;
+assign paddrout =paddr;
+assign pselxout =pselx;
+assign pwdataout =pwdata;
+assign penableout =penable;
+
+always@(*)
+begin 
+if(!pwrite&&penable)
+prdata={$random}%256;
+else
+prdata=32'h0;
+end
+
+endmodule
