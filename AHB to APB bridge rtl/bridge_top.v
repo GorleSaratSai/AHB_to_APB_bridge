fd3e373ef76c71a84585ea698e1127348d1362fe
GorleSaratSai <140937178+GorleSaratSai@users.noreply.github.com>
2023-07-30 15:05:07 +0530

    Update bridge_top.v

--- AHB to APB bridge rtl/bridge_top.v
+++ AHB to APB bridge rtl/bridge_top.v
@@ -19,7 +19,7 @@ module bridge_top(hclk,hresetn,hwrite,hreadyin,htrans,hwdata,haddr,prdata,penabl
   wire [31:0] hwdata1;
   wire [31:0] hwdata2;
   wire [31:0] hwdata3;
- wire [31:0] hwdata4;
+  wire [31:0] hwdata4;
   wire [31:0] haddr1;
   wire [31:0] haddr2;
   wire [31:0] haddr3;
@@ -28,8 +28,8 @@ module bridge_top(hclk,hresetn,hwrite,hreadyin,htrans,hwdata,haddr,prdata,penabl
   wire hwritereg,hwritereg1,hwritereg2,hwritereg3;
   wire valid;
   
-  ahb_slave_interface A1(hclk,hresetn,hwrite,hreadyin,htrans,hwdata,haddr,prdata,hwritereg,hwritereg1,hwritereg3,hwritereg2,valid,hwdata1,hwdata2,hwdata3,hwdata4,haddr1,haddr2,haddr3,haddr4,hrdata,hresp,tempselx);
+    ahb_slave_interface A1(hclk,hresetn,hwrite,hreadyin,htrans,hwdata,haddr,prdata,hwritereg,hwritereg1,hwritereg3,hwritereg2,valid,hwdata1,hwdata2,hwdata3,hwdata4,haddr1,haddr2,haddr3,haddr4,hrdata,hresp,tempselx);
   
-  apb_controller A2(hclk,hresetn,hwritereg,hwritereg1,hwritereg2,hwritereg3,hwrite,valid,haddr,hwdata,hwdata1,hwdata2,hwdata3,hwdata4,haddr1,haddr2,haddr3,haddr4,prdata,tempselx,penable,pwrite,hreadyout,pselx,paddr,pwdata);
+    apb_controller A2(hclk,hresetn,hwritereg,hwritereg1,hwritereg2,hwritereg3,hwrite,valid,haddr,hwdata,hwdata1,hwdata2,hwdata3,hwdata4,haddr1,haddr2,haddr3,haddr4,prdata,tempselx,penable,pwrite,hreadyout,pselx,paddr,pwdata);
   
   endmodule
